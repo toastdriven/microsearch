47f47bc08ec7a7d4a24831a3c364a3952fdabd60
Daniel Lindsley <daniel@toastdriven.com>
2011-12-10 02:35:47 -0600

    Added ``EnglishTokenizer`` & ``EdgeNgramGenerator`` for basic text processing.

--- microsearch.py
+++ microsearch.py
@@ -15,4 +15,84 @@ __license__ = 'BSD'
 __version__ = (0, 1, 0)
 
 
+FRONT = 'front'
+BACK = 'back'
+
+
+class EnglishTokenizer(object):
+    separators = set([
+        '\n', '\r', ' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(',
+        ')', '+', '=', '{', '[', '}', ']', '|', '\\', ':', ';', '"', '<', ',',
+        '>', '.', '?', '/',
+        # Leaving these alone, as they commonly combine words.
+        # '_', '-', '\'',
+    ])
+    stopwords = set([
+        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by',
+        'for', 'if', 'in', 'into', 'is', 'it',
+        'no', 'not', 'of', 'on', 'or', 's', 'such',
+        't', 'that', 'the', 'their', 'then', 'there', 'these',
+        'they', 'this', 'to', 'was', 'will', 'with'
+    ])
+
+    def __init__(self, text):
+        self.text = text.lower()
+        self.offset = -1
+
+    def __iter__(self):
+        current_token = []
+
+        for letter in self.text:
+            if letter in self.separators:
+                if len(current_token):
+                    token = ''.join(current_token)
+
+                    if not token in self.stopwords:
+                        yield token
+
+                # Reset the current token to nothing
+                current_token = []
+            else:
+                current_token.append(letter)
+
+        raise StopIteration()
+
+    def next(self):
+        for offset, token in enumerate(self):
+            if offset > self.offset:
+                self.offset = offset
+                return token
+
+
+class EdgeNgramGenerator(object):
+    def __init__(self, token, min_size=3, max_size=15, side=FRONT):
+        self.token = token
+        self.min_size = min_size
+        self.max_size = max_size
+        self.side = side
+
+    def run(self):
+        text = self.token
+        grams = []
+        length = len(text)
+
+        for slice_len in range(self.min_size, self.max_size + 1):
+            if self.side == FRONT:
+                current_gram = text[:slice_len]
+            else:
+                current_gram = text[length - slice_len:]
+
+            if len(current_gram) < slice_len:
+                # We've got all the grams covered.
+                break
+
+            grams.append(current_gram)
+
+        # If we haven't got any grams but there was valid text there, we have
+        # text that was smaller than the ``min_size``. Toss the whole thing
+        # into the ``grams`` so the word doesn't get skipped.
+        if not grams and length:
+            grams.append(text)
+
+        return grams
 
--- tests.py
+++ tests.py
@@ -1,5 +1,59 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 import unittest2
+import microsearch
 
+
+class EnglishTokenizerTestCase(unittest2.TestCase):
+    def test_init(self):
+        et = microsearch.EnglishTokenizer('Hello world!')
+        self.assertEqual(et.text, 'hello world!')
+
+    def test_iter(self):
+        et = microsearch.EnglishTokenizer('Hello world!')
+        self.assertEqual(et.next(), 'hello')
+        self.assertEqual(et.next(), 'world')
+
+    def test_all(self):
+        et = microsearch.EnglishTokenizer('Hello world!')
+        self.assertEqual(list(et), ['hello', 'world'])
+
+    def test_separators_stopwords(self):
+        et = microsearch.EnglishTokenizer('Hello world! I\'m like, totally happy to meet you; it\'s my pleasure. Come, sit by the fire...')
+        self.assertEqual(list(et), ['hello', 'world', 'i\'m', 'like', 'totally', 'happy', 'meet', 'you', 'it\'s', 'my', 'pleasure', 'come', 'sit', 'fire'])
+
+
+class EdgeNgramGeneratorTestCase(unittest2.TestCase):
+    def test_simple_init(self):
+        eng = microsearch.EdgeNgramGenerator('Hello')
+        self.assertEqual(eng.token, 'Hello')
+        self.assertEqual(eng.min_size, 3)
+        self.assertEqual(eng.max_size, 15)
+        self.assertEqual(eng.side, microsearch.FRONT)
+
+    def test_complex_init(self):
+        eng = microsearch.EdgeNgramGenerator('Hello', min_size=8, side=microsearch.BACK)
+        self.assertEqual(eng.token, 'Hello')
+        self.assertEqual(eng.min_size, 8)
+        self.assertEqual(eng.max_size, 15)
+        self.assertEqual(eng.side, microsearch.BACK)
+
+    def test_simple_run(self):
+        eng = microsearch.EdgeNgramGenerator('Hello')
+        self.assertEqual(eng.run(), ['Hel', 'Hell', 'Hello'])
+
+    def test_alternate_gram_size(self):
+        eng = microsearch.EdgeNgramGenerator('Hello', min_size=1, max_size=2)
+        self.assertEqual(eng.run(), ['H', 'He'])
+
+    def test_gram_size_bigger_than_text(self):
+        eng = microsearch.EdgeNgramGenerator('Hello', min_size=8)
+        self.assertEqual(eng.run(), ['Hello'])
+
+    def test_simple_back(self):
+        eng = microsearch.EdgeNgramGenerator('Hello', side=microsearch.BACK)
+        self.assertEqual(eng.run(), ['llo', 'ello', 'Hello'])
+
+    def test_complex_back(self):
+        eng = microsearch.EdgeNgramGenerator('Hello', min_size=2, max_size=4, side=microsearch.BACK)
+        self.assertEqual(eng.run(), ['lo', 'llo', 'ello'])
