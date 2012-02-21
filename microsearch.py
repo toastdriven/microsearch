a0d50d0b7c224b1389ae0ba8735dc4fee299335e
Daniel Lindsley <daniel@toastdriven.com>
2012-02-21 03:13:27 -0600

    Converted to a class-based setup.

--- microsearch.py
+++ microsearch.py
@@ -46,151 +46,160 @@ __license__ = 'BSD'
 __version__ = (0, 1, 0)
 
 
-# A fairly standard list of "stopwords", which are words that contribute little
-# to relevance (since they are so common in English) & are to be ignored.
-STOP_WORDS = set([
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by',
-    'for', 'if', 'in', 'into', 'is', 'it',
-    'no', 'not', 'of', 'on', 'or', 's', 'such',
-    't', 'that', 'the', 'their', 'then', 'there', 'these',
-    'they', 'this', 'to', 'was', 'will', 'with'
-])
-PUNCTUATION = re.compile('[~`!@#$%^&*()+={\[}\]|\\:;"\',<.>/?]')
+class Microsearch(object):
+    # A fairly standard list of "stopwords", which are words that contribute little
+    # to relevance (since they are so common in English) & are to be ignored.
+    STOP_WORDS = set([
+        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by',
+        'for', 'if', 'in', 'into', 'is', 'it',
+        'no', 'not', 'of', 'on', 'or', 's', 'such',
+        't', 'that', 'the', 'their', 'then', 'there', 'these',
+        'they', 'this', 'to', 'was', 'will', 'with'
+    ])
+    PUNCTUATION = re.compile('[~`!@#$%^&*()+={\[}\]|\\:;"\',<.>/?]')
+
+    def __init__(self, base_directory):
+        self.base_directory = base_directory
+        self.index_path = os.path.join(self.base_directory, 'index')
+        self.docs_path = os.path.join(self.base_directory, 'documents')
+        self.setup()
+
+    def setup(self):
+        if not os.path.exists(self.base_directory):
+            os.makedirs(self.base_directory)
+
+        if not os.path.exists(self.index_path):
+            os.makedirs(self.index_path)
+
+        if not os.path.exists(self.docs_path):
+            os.makedirs(self.docs_path)
+
+        return True
 
 
-# ==============================
-# Tokenization & Term Generation
-# ==============================
+    # ==============================
+    # Tokenization & Term Generation
+    # ==============================
 
-def make_tokens(blob):
-    # Kill the punctuation.
-    blob = PUNCTUATION.sub(' ', blob)
-    tokens = []
+    def make_tokens(self, blob):
+        # Kill the punctuation.
+        blob = self.PUNCTUATION.sub(' ', blob)
+        tokens = []
 
-    # Split on spaces.
-    for token in blob.split():
-        # Make sure everything is in lowercase & whitespace removed.
-        token = token.lower().strip()
+        # Split on spaces.
+        for token in blob.split():
+            # Make sure everything is in lowercase & whitespace removed.
+            token = token.lower().strip()
 
-        if not token in STOP_WORDS:
-            tokens.append(token)
+            if not token in self.STOP_WORDS:
+                tokens.append(token)
 
-    return tokens
+        return tokens
 
+    def make_ngrams(self, tokens, min_gram=3, max_gram=6):
+        terms = {}
 
-def make_ngrams(tokens, min_gram=3, max_gram=6):
-    terms = {}
+        for position, token in enumerate(tokens):
+            for window_length in range(min_gram, min(max_gram + 1, len(token) + 1)):
+                # Assuming "front" grams.
+                gram = token[:window_length]
+                terms.setdefault(gram, [])
 
-    for position, token in enumerate(tokens):
-        for window_length in range(min_gram, min(max_gram + 1, len(token) + 1)):
-            # Assuming "front" grams.
-            gram = token[:window_length]
-            terms.setdefault(gram, [])
+                if not position in terms[gram]:
+                    terms[gram].append(position)
 
-            if not position in terms[gram]:
-                terms[gram].append(position)
-
-    return terms
-
-
-# ================
-# Segment Handling
-# ================
-
-def make_segment_name(term, length=6):
-    # Make sure it's ASCII to appease the hashlib gods.
-    term = term.encode('ascii', errors='ignore')
-    # We hash & slice the term to get a small-ish number of fields
-    # and good distribution between them.
-    hashed = hashlib.md5(term).hexdigest()
-    return "{0}.index".format(hashed[:length])
+        return terms
 
 
-def parse_record(line):
-    return line.rstrip().split('\t', 1)
+    # ================
+    # Segment Handling
+    # ================
 
+    def make_segment_name(self, term, length=6):
+        # Make sure it's ASCII to appease the hashlib gods.
+        term = term.encode('ascii', errors='ignore')
+        # We hash & slice the term to get a small-ish number of fields
+        # and good distribution between them.
+        hashed = hashlib.md5(term).hexdigest()
+        return "{0}.index".format(hashed[:length])
 
-def make_record(term, term_info):
-    return "{0}\t{1}\n".format(term, json.dumps(term_info))
+    def parse_record(self, line):
+        return line.rstrip().split('\t', 1)
 
+    def make_record(self, term, term_info):
+        return "{0}\t{1}\n".format(term, json.dumps(term_info))
 
-def save_segment(term, term_info):
-    seg_name = make_segment_name(term)
-    new_seg_file = tempfile.NamedTemporaryFile(delete=False)
-    written = False
+    def save_segment(self, term, term_info):
+        seg_name = self.make_segment_name(term)
+        new_seg_file = tempfile.NamedTemporaryFile(delete=False)
+        written = False
 
-    with open(seg_name, 'r') as seg_file:
-        for line in seg_file:
-            seg_term, term_info = parse_record(line)
+        with open(seg_name, 'r') as seg_file:
+            for line in seg_file:
+                seg_term, term_info = self.parse_record(line)
 
-            if not written and seg_term > term:
-                # We're at the alphabetical location & need to insert.
-                new_line = make_record(term, term_info)
-                new_seg_file.write(new_line)
-            elif seg_term == term:
-                # Overwrite the line for the update.
-                line = make_record(term, term_info)
+                if not written and seg_term > term:
+                    # We're at the alphabetical location & need to insert.
+                    new_line = self.make_record(term, term_info)
+                    new_seg_file.write(new_line)
+                elif seg_term == term:
+                    # Overwrite the line for the update.
+                    line = self.make_record(term, term_info)
 
-            # Either we haven't reached it alphabetically or we're well-past.
-            # Write the line.
-            new_seg_file.write(line)
+                # Either we haven't reached it alphabetically or we're well-past.
+                # Write the line.
+                new_seg_file.write(line)
 
-    # Atomically move it into place.
-    new_seg_file.close()
-    os.rename(new_seg_file.name, seg_name)
-    return True
+        # Atomically move it into place.
+        new_seg_file.close()
+        os.rename(new_seg_file.name, seg_name)
+        return True
 
+    def load_segment(self, term):
+        seg_name = self.make_segment_name(term)
 
-def load_segment(term):
-    seg_name = make_segment_name(term)
+        if not os.path.exists(seg_name):
+            return {}
 
-    if not os.path.exists(seg_name):
+        with open(seg_name, 'r') as seg_file:
+            for line in seg_file:
+                seg_term, term_info = self.parse_record(line)
+
+                if seg_term == term:
+                    # Found it.
+                    return json.loads(term_info)
+
         return {}
 
-    with open(seg_name, 'r') as seg_file:
-        for line in seg_file:
-            seg_term, term_info = parse_record(line)
-
-            if seg_term == term:
-                # Found it.
-                return json.loads(term_info)
-
-    return {}
+    def index(self, doc_id, document):
+        pass
 
 
-def index(doc_id, document):
-    pass
+    # =========
+    # Searching
+    # =========
 
+    def parse_query(self, query):
+        tokens = self.make_tokens(query)
+        return self.make_ngrams(tokens)
 
-# =========
-# Searching
-# =========
+    def collect_results(self, terms):
+        matches = {}
 
-def parse_query(query):
-    tokens = make_tokens(query)
-    return make_ngrams(tokens)
+        for term in terms:
+            matches[term] = self.load_segment(term)
 
+        return matches
 
-def collect_results(terms):
-    matches = {}
+    def bm25_relevance(self, terms, matches, current_docs, total_docs, b=0, k=1.2):
+        score = b
 
-    for term in terms:
-        matches[term] = load_segment(term)
+        for term in terms:
+            idf = math.log((total_docs - matches[term] + 1) / matches[term]) / math.log(1.0 + total_docs)
+            score = score + current_docs[term] * idf / (current_docs[term] + k)
 
-    return matches
+        return 0.5 + score / (2 * len(terms))
 
-
-def bm25_relevance(terms, matches, current_docs, total_docs, b=0, k=1.2):
-    score = b
-
-    for term in terms:
-        idf = math.log((total_docs - matches[term] + 1) / matches[term]) / math.log(1.0 + total_docs)
-        score = score + current_docs[term] * idf / (current_docs[term] + k)
-
-    return 0.5 + score / (2 * len(terms))
-
-
-def search(query):
-    terms = parse_query(query)
-    results = collect_results(terms)
-
+    def search(self, query):
+        terms = self.parse_query(query)
+        results = self.collect_results(terms)
--- tests.py
+++ tests.py
@@ -10,16 +10,18 @@ class MicrosearchTestCase(unittest.TestCase):
         self.base = os.path.join('/tmp', 'microsearch_tests')
         shutil.rmtree(self.base, ignore_errors=True)
 
+        self.micro = microsearch.Microsearch(self.base)
+
     def tearDown(self):
         shutil.rmtree(self.base, ignore_errors=True)
         super(MicrosearchTestCase, self).tearDown()
 
     def test_make_tokens(self):
-        self.assertEqual(microsearch.make_tokens('Hello world'), ['hello', 'world'])
-        self.assertEqual(microsearch.make_tokens("This is a truly splendid example of some tokens. Top notch, really."), ['truly', 'splendid', 'example', 'some', 'tokens', 'top', 'notch', 'really'])
+        self.assertEqual(self.micro.make_tokens('Hello world'), ['hello', 'world'])
+        self.assertEqual(self.micro.make_tokens("This is a truly splendid example of some tokens. Top notch, really."), ['truly', 'splendid', 'example', 'some', 'tokens', 'top', 'notch', 'really'])
 
     def test_make_ngrams(self):
-        self.assertEqual(microsearch.make_ngrams(['hello', 'world']), {
+        self.assertEqual(self.micro.make_ngrams(['hello', 'world']), {
             'hel': [0],
             'hell': [0],
             'hello': [0],
@@ -27,7 +29,7 @@ class MicrosearchTestCase(unittest.TestCase):
             'worl': [1],
             'world': [1],
         })
-        self.assertEqual(microsearch.make_ngrams(['truly', 'splendid', 'example', 'some', 'tokens', 'top', 'notch', 'really']), {
+        self.assertEqual(self.micro.make_ngrams(['truly', 'splendid', 'example', 'some', 'tokens', 'top', 'notch', 'really']), {
             'tru': [0],
             'trul': [0],
             'truly': [0],
@@ -56,19 +58,74 @@ class MicrosearchTestCase(unittest.TestCase):
         })
 
     def test_make_segment_name(self):
-        self.assertEqual(microsearch.make_segment_name('hello'), '5d4140.index')
-        self.assertEqual(microsearch.make_segment_name('world'), '7d7930.index')
-        self.assertEqual(microsearch.make_segment_name('truly'), 'f499b3.index')
-        self.assertEqual(microsearch.make_segment_name('splendid'), '291e4e.index')
-        self.assertEqual(microsearch.make_segment_name('example'), '1a79a4.index')
-        self.assertEqual(microsearch.make_segment_name('some'), '03d59e.index')
-        self.assertEqual(microsearch.make_segment_name('tokens'), '25d718.index')
-        self.assertEqual(microsearch.make_segment_name('top'), 'b28354.index')
-        self.assertEqual(microsearch.make_segment_name('notch'), '9ce862.index')
-        self.assertEqual(microsearch.make_segment_name('really'), 'd2d92e.index')
+        self.assertEqual(self.micro.make_segment_name('hello'), '5d4140.index')
+        self.assertEqual(self.micro.make_segment_name('world'), '7d7930.index')
+        self.assertEqual(self.micro.make_segment_name('truly'), 'f499b3.index')
+        self.assertEqual(self.micro.make_segment_name('splendid'), '291e4e.index')
+        self.assertEqual(self.micro.make_segment_name('example'), '1a79a4.index')
+        self.assertEqual(self.micro.make_segment_name('some'), '03d59e.index')
+        self.assertEqual(self.micro.make_segment_name('tokens'), '25d718.index')
+        self.assertEqual(self.micro.make_segment_name('top'), 'b28354.index')
+        self.assertEqual(self.micro.make_segment_name('notch'), '9ce862.index')
+        self.assertEqual(self.micro.make_segment_name('really'), 'd2d92e.index')
 
     def test_parse_record(self):
-        self.assertEqual(microsearch.parse_record('hello\t{"abc": [1, 2, 3]}\n'), ['hello', '{"abc": [1, 2, 3]}'])
+        self.assertEqual(self.micro.parse_record('hello\t{"abc": [1, 2, 3]}\n'), ['hello', '{"abc": [1, 2, 3]}'])
 
     def test_make_record(self):
-        self.assertEqual(microsearch.make_record('hello', {"abc": [1, 2, 3]}), 'hello\t{"abc": [1, 2, 3]}\n')
+        self.assertEqual(self.micro.make_record('hello', {"abc": [1, 2, 3]}), 'hello\t{"abc": [1, 2, 3]}\n')
+
+    def test_save_segment(self):
+        pass
+
+    def test_load_segment(self):
+        pass
+
+    def test_parse_query(self):
+        self.assertEqual(self.micro.parse_query('Hello world!'), {
+            'hel': [0],
+            'hell': [0],
+            'hello': [0],
+            'wor': [1],
+            'worl': [1],
+            'world': [1],
+        })
+
+    def test_collect_results(self):
+        pass
+
+    def test_bm25_relevance(self):
+        terms = ['hello']
+        matching_docs = {
+            'hello': 7,
+        }
+        current_doc_occurances = {
+            'hello': 3,
+        }
+        total_docs = 17
+        relevance = self.micro.bm25_relevance(terms, matching_docs, current_doc_occurances, total_docs)
+        self.assertEqual("{:.2f}".format(relevance), '0.56', 'This fails on 2.X but should pass on Python 3.')
+
+        terms = ['hello']
+        matching_docs = {
+            'hello': 25,
+        }
+        current_doc_occurances = {
+            'hello': 5,
+        }
+        total_docs = 175
+        relevance = self.micro.bm25_relevance(terms, matching_docs, current_doc_occurances, total_docs)
+        self.assertEqual("{:.2f}".format(relevance), '0.64', 'This fails on 2.X but should pass on Python 3.')
+
+        terms = ['hello', 'world']
+        matching_docs = {
+            'hello': 25,
+            'world': 7,
+        }
+        current_doc_occurances = {
+            'hello': 5,
+            'world': 3,
+        }
+        total_docs = 175
+        relevance = self.micro.bm25_relevance(terms, matching_docs, current_doc_occurances, total_docs)
+        self.assertEqual("{:.2f}".format(relevance), '0.68', 'This fails on 2.X but should pass on Python 3.')
